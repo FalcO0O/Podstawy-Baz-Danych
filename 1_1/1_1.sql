21d4a07eeaaa7f4a5b759db4a7412237dd528842
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2024-11-06 00:59:28 +0100

    clearing up

--- 1_1/1_1.sql
+++ 1_1/1_1.sql
@@ -1,3 +1,22 @@
+select CompanyName, Address from Customers where city = 'London'
+select CompanyName, Address from Customers where country = 'France' or country = 'Spain'
+select ProductName, UnitPrice from Products where UnitPrice > 20 and UnitPrice < 30
+select * from Categories
+select ProductName, UnitPrice from Products where CategoryID = 'Meat/Poultry'
+select ProductName, UnitsInStock from Products where SupplierID = ''
+select * from Products where QuantityPerUnit LIKE '%bottle%'
+select Title from Employees where LastName like '[B-L]%'
+select Title from Employees where LastName like '[B,L]%'
+select CategoryName from Categories where Description like '%,%'
+select CompanyName from Customers where CompanyName like '%Store%'
+select * from Products where UnitPrice not between 10 and 20
+select CompanyName, Country from Suppliers where Country = 'Japan' or Country = 'Italy'
+select CompanyName, Country from Suppliers where Country in ('Japan', 'Italy')
+select orderID, orderDate, CustomerID from Orders where shipCountry = 'Argentina' and ShippedDate is NULL
+
+
+
+
 select * from Products where QuantityPerUnit like '%bottle%'
 select Title from Employees where LastName like '[B-L]%'
 select Title from Employees where LastName like '[BL]%'
@@ -27,4 +46,3 @@ select avg(UnitPrice) from Products where QuantityPerUnit like '%bottle%'
 select * from products where UnitPrice > avg(UnitPrice) /*NOO*/
 select * from products where UnitPrice > (select avg(unitprice) from Products /*YESSS*/
 
-select round(sum(unitPrice*Quantity*(1-Discount)),2) from [Order Details] where orderid = 10250
