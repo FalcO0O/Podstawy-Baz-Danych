9a66ebc62dfba53a4d2b2d581295c4f2d0135c1c
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2024-11-24 20:38:49 +0100

    3_1 and 4_1

--- 3_1/3_1.sql
+++ 3_1/3_1.sql
@@ -1,14 +1,169 @@
 
+-- æw 1
+
+-- 1
+SELECT 
+    O.OrderID,
+    C.CompanyName AS CustomerName,
+    SUM(OD.Quantity) AS TotalUnits
+FROM Orders AS O
+INNER JOIN [Order Details] AS OD ON O.OrderID = OD.OrderID
+INNER JOIN Customers AS C ON O.CustomerID = C.CustomerID
+GROUP BY O.OrderID, C.CompanyName
+ORDER BY O.OrderID;
+
+-- 2
+
+SELECT Orders.OrderID, C.CompanyName AS CustomerName,  SUM(Quantity) AS 'lzj' 
+FROM Orders
+INNER JOIN [Order Details] OD on Orders.OrderID = OD.OrderID
+INNER JOIN Customers AS C ON Orders.CustomerID = C.CustomerID
+GROUP BY Orders.OrderID, C.CompanyName
+HAVING SUM(Quantity) > 250;
+
+
+-- 3
+
+SELECT Orders.OrderID, SUM(UnitPrice*Quantity*(1-Discount)) AS 'Wartosc',
+C.CompanyName
+FROM Orders
+INNER JOIN [Order Details] [O D] on Orders.OrderID = [O D].OrderID
+INNER JOIN Customers C on Orders.CustomerID = C.CustomerID
+GROUP BY Orders.OrderID, C.CompanyName
+ORDER BY Orders.OrderID;
+
+-- 4
+
+SELECT Orders.OrderID, SUM(UnitPrice*Quantity*(1-Discount)) AS 'Wartosc',
+C.CompanyName
+FROM Orders
+INNER JOIN [Order Details] [O D] on Orders.OrderID = [O D].OrderID
+INNER JOIN Customers C on Orders.CustomerID = C.CustomerID
+GROUP BY Orders.OrderID, C.CompanyName
+HAVING SUM(Quantity) > 250
+ORDER BY Orders.OrderID;
+
+-- 5
+
+SELECT Orders.OrderID, SUM(UnitPrice*Quantity*(1-Discount)) AS 'Wartosc',
+C.CompanyName, E.FirstName + ' ' + E.LastName AS 'Pracownik'
+FROM Orders
+INNER JOIN [Order Details] [O D] on Orders.OrderID = [O D].OrderID
+INNER JOIN Customers C on Orders.CustomerID = C.CustomerID
+INNER JOIN Employees E ON E.EmployeeID = Orders.EmployeeID
+GROUP BY Orders.OrderID, C.CompanyName, E.FirstName + ' ' + E.LastName
+HAVING SUM(Quantity) > 250
+ORDER BY Orders.OrderID;
 
 ------------------------------------------------------------------------
--- zad 4
 
-select (FirstName + ' ' + LastName), round(sum(UnitPrice * Quantity*(1-Discount)),2)
-from Employees E
-join Orders O on E.EmployeeID = O.EmployeeID
-join [Order Details] OD on O.OrderID = OD.Discount
-group by E.EmployeeID, (FirstName + ' ' + LastName)
-order by 2 desc
+-- æw 2
+
+-- 1
+
+SELECT CategoryName, SUM(Quantity) AS 'lzj'
+FROM Categories
+INNER JOIN Products
+ON Products.CategoryID = Categories.CategoryID
+INNER JOIN [Order Details] AS OD
+ON OD.ProductID = Products.ProductID
+GROUP BY CategoryName;
+
+-- 2
+
+SELECT CategoryName, SUM(OD.UnitPrice*Quantity*(1-Discount)) AS 'lwz'
+FROM Categories
+INNER JOIN Products
+ON Products.CategoryID = Categories.CategoryID
+INNER JOIN [Order Details] AS OD
+ON OD.ProductID = Products.ProductID
+GROUP BY CategoryName;
+-- 3
+
+--a
+SELECT CategoryName, SUM(OD.UnitPrice*Quantity*(1-Discount)) AS 'lwz'
+FROM Categories
+INNER JOIN Products
+ON Products.CategoryID = Categories.CategoryID
+INNER JOIN [Order Details] AS OD
+ON OD.ProductID = Products.ProductID
+GROUP BY CategoryName
+ORDER BY lwz DESC;
+
+--b
+SELECT CategoryName, SUM(OD.UnitPrice*Quantity*(1-Discount)) AS 'lwz',
+SUM(Quantity) AS 'llzj'
+FROM Categories
+INNER JOIN Products
+ON Products.CategoryID = Categories.CategoryID
+INNER JOIN [Order Details] AS OD
+ON OD.ProductID = Products.ProductID
+GROUP BY CategoryName
+ORDER BY llzj;
+-- 4
+
+SELECT 
+    O.OrderID,
+    SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) + O.Freight AS OrderValue
+FROM Orders AS O
+INNER JOIN [Order Details] AS OD ON O.OrderID = OD.OrderID
+GROUP BY O.OrderID, O.Freight
+ORDER BY O.OrderID;
+
+------------------------------------------------------------------------
+
+-- æw 3
+
+-- 1
+SELECT CompanyName, COUNT(OrderID) AS 'amount of orders'
+FROM Shippers
+INNER JOIN Orders
+ON Orders.ShipVia = Shippers.ShipperID
+WHERE year(Orders.ShippedDate) = 1997
+GROUP BY CompanyName
+
+-- 2
+
+SELECT TOP 1 CompanyName, COUNT(OrderID) AS 'amount of orders'
+FROM Shippers
+INNER JOIN Orders
+ON Orders.ShipVia = Shippers.ShipperID
+WHERE year(Orders.ShippedDate) = 1997
+GROUP BY CompanyName
+ORDER BY [amount of orders] DESC
+
+
+-- 3
+SELECT FirstName + '' + LastName, SUM(OD.UnitPrice*Quantity*(1-Discount))
+FROM Employees
+INNER JOIN Orders
+ON Orders.EmployeeID = Employees.EmployeeID
+INNER JOIN [Order Details] AS OD
+ON OD.OrderID = Orders.OrderID
+GROUP BY FirstName + '' + LastName;
+
+-- 4
+SELECT TOP 1 FirstName + ' ' + LastName AS name, COUNT(OrderID)
+FROM Employees
+INNER JOIN Orders
+ON Orders.EmployeeID = Employees.EmployeeID
+GROUP BY FirstName + ' ' + LastName
+ORDER BY COUNT(OrderID) DESC; 
+
+-- 5
+SELECT TOP 1 FirstName + '' + LastName,
+SUM(OD.UnitPrice*Quantity*(1-Discount)) AS 'lwz'
+FROM Employees
+INNER JOIN Orders
+ON Orders.EmployeeID = Employees.EmployeeID
+INNER JOIN [Order Details] AS OD
+ON OD.OrderID = Orders.OrderID
+WHERE year(Orders.ShippedDate) = 1997
+GROUP BY FirstName + '' + LastName
+ORDER BY lwz DESC;
+
+------------------------------------------------------------------------
+-- æw 4
 
 --a)
 
--- 4_1/4_1.sql
+++ 4_1/4_1.sql
@@ -1,6 +1,14 @@
+use Northwind
+
 -- Æw. 1
 
--- 1 do domu
+-- 1
+
+SELECT DISTINCT Customers.CompanyName, Customers.Phone
+FROM Customers
+INNER JOIN Orders O on Customers.CustomerID = O.CustomerID
+INNER JOIN Shippers S on O.ShipVia = S.ShipperID
+WHERE year(ShippedDate) = 1997 AND S.CompanyName = 'United Package';
 
 -- 2 
 
@@ -17,7 +25,6 @@ and c3.CategoryName = 'Confections'
 
 -- 3
 
-
 select c.companyName, c.Phone
 from Customers c
 where c.CustomerID in
@@ -29,7 +36,13 @@ join Categories c3 on p.CategoryID = c3.CategoryID
 and not c3.CategoryName = 'Confections'
 )
 
+
+--------------------------------------------------------------------------
+
+
 -- æw. 2
+
+-- 1
 
 SELECT DISTINCT P.ProductName, quantity
 FROM [order details] AS ord1
@@ -41,20 +54,60 @@ WHERE quantity = (
 		ord2.productid 
 )
 
+-- 2
+
+SELECT P.ProductName, P.UnitPrice
+FROM Products P
+WHERE P.UnitPrice < (
+ SELECT AVG(UnitPrice) FROM Products
+ ); 
+
+ -- 3
+
+SELECT P.ProductID, P.ProductName
+FROM Products AS P
+WHERE P.UnitPrice < (
+SELECT AVG(UnitPrice)
+	FROM Products AS P2
+	WHERE P2.CategoryID = P.CategoryID
+) 
+
+--------------------------------------------------------------------------
+
+-- æw 3
+
+-- 1
+
+SELECT P.ProductName, P.UnitPrice, 
+	(SELECT AVG(UnitPrice)
+	FROM Products) AS 'averagePrice',
+	P.UnitPrice - (SELECT AVG(UnitPrice) FROM Products) AS 'difference'
+FROM Products AS P;
+
+
+-- 2
+
+SELECT 
+(SELECT C.CategoryName
+	FROM Categories AS C
+	WHERE C.CategoryID = P.CategoryID) AS 'CategoryName',
+P.ProductName, P.UnitPrice,
+(SELECT AVG(P2.UnitPrice)
+	FROM Products AS P2
+	WHERE P2.CategoryID = P.CategoryID) AS 'AveragePriceByCategory',
+	P.UnitPrice - (SELECT AVG(P2.UnitPrice)
+FROM Products AS P2
+WHERE P2.CategoryID = P.CategoryID) AS 'difference'
+FROM Products AS P
+
 --------------------------------------------------------------------------
 -- æw 4
 
--- 1 - Podaj ³¹czn¹ wartoœæ zamówienia o numerze 10250 (uwzglêdnij cenê za przesy³kê)
+-- 1 - Podaj ³¹czn¹ wartoœæ zamówienia o numerze 10250 !
 
 select round(sum(quantity * unitprice * (1-Discount)) + (select freight from orders where orderid = 10250),2) as '10250'
 from [Order Details]
 where OrderID = 10250
-
-select OD.OrderID, round(sum((UnitPrice * Quantity) * (1-Discount) + 
-(select freight from orders where orders.OrderID = 10250)),2)
-from [Order Details] OD
-where OD.OrderID = 10250
-group by OD.OrderID
 
 -- 2
 select o.orderId, round((select sum(UnitPrice * quantity *(1-discount)) from [Order details] as od
@@ -69,21 +122,122 @@ SELECT DISTINCT CustomerID
 FROM Orders
 WHERE YEAR(OrderDate) = 1997
 );
+
+-- sprawdzenie
 -- 86
 SELECT DISTINCT CustomerID
 from Orders
 WHERE YEAR(OrderDate) = 1997
+
 -- 91
 select distinct CustomerID
 	FROM Customers
 
+-- 4
+
+SELECT T.ProductName
+FROM (SELECT DISTINCT p.ProductID as ProductID, p.ProductName as ProductName, o.CustomerID as CustomerID
+FROM Products p
+INNER JOIN [Order Details] od ON p.ProductID = od.ProductID
+INNER JOIN Orders o ON od.OrderID = o.OrderID 
+GROUP BY p.ProductID, p.ProductName, o.CustomerID) as T 
+GROUP BY T.ProductID, T.ProductName
+HAVING COUNT (T.ProductID) >= 2
 
 -- æw 5
 
-select firstName, LastName, (select (unitPrice * quantity * (1-discount)) from [Order Details] od
-join orders o on o.OrderID = od.OrderID
-join Employees e on o.OrderDate = e.EmployeeID
-) from Employees
+-- 1
+SELECT (SELECT (FirstName +' '+ LastName) 
+FROM Employees 
+WHERE Orders.EmployeeID=Employees.EmployeeID) as 'Pracownik',
+ROUND(SUM(Quantity*UnitPrice*(1-Discount)+Freight), 2) as 'Wartosc'
+FROM [Order Details]
+INNER JOIN Orders ON [Order Details].OrderID = Orders.OrderID
+GROUP BY EmployeeID
 
--- zadanie domowe:
--- dokoñczyæ 3_1 - 4_1
+-- 2
+
+SELECT TOP 1 E.FirstName + ' ' + e.LastName as 'name', (
+ SELECT SUM(OD.UnitPrice*od.quantity*(1-od.Discount))
+ from Orders AS O
+ INNER JOIN [Order Details] as OD ON O.OrderID = OD.OrderID
+ WHERE E.EmployeeID = O.EmployeeID AND year(O.ShippedDate) = 1997
+ ) AS 'value'
+FROM Employees as e
+ORDER BY value DESC
+
+
+-- 3
+
+-- a) którzy maj¹ podw³adnych
+
+SELECT E.FirstName + ' ' + E.LastName AS 'name', 
+(SELECT SUM(OD.UnitPrice*od.quantity*(1-od.Discount))
+from Orders AS O
+INNER JOIN [Order Details] as OD ON O.OrderID = OD.OrderID
+WHERE E.EmployeeID = O.EmployeeID) + (SELECT sum(O.Freight)
+from Orders as o
+WHERE o.EmployeeID = e.EmployeeID)
+FROM Employees AS E
+WHERE e.EmployeeID IN (select distinct a.EmployeeID
+from Employees as a
+inner join Employees as b on a.EmployeeID = b.ReportsTo)
+
+
+-- b) którzy nie maj¹ podw³adnych
+
+SELECT E.FirstName + ' ' + E.LastName AS 'name', 
+(SELECT SUM(OD.UnitPrice*od.quantity*(1-od.Discount))
+from Orders AS O
+INNER JOIN [Order Details] as OD ON O.OrderID = OD.OrderID
+WHERE E.EmployeeID = O.EmployeeID) + (SELECT sum(O.Freight)
+from Orders as o
+WHERE o.EmployeeID = e.EmployeeID)
+FROM Employees AS E
+WHERE e.EmployeeID IN (select distinct a.EmployeeID
+from Employees as a
+left join Employees as b on a.EmployeeID = b.ReportsTo
+where b.EmployeeID is null);
+
+-- 4
+
+-- a)
+SELECT 
+    E.FirstName + ' ' + E.LastName AS 'name', 
+    (SELECT SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount))
+     FROM Orders AS O
+     INNER JOIN [Order Details] AS OD ON O.OrderID = OD.OrderID
+     WHERE E.EmployeeID = O.EmployeeID) +
+    (SELECT SUM(O.Freight)
+     FROM Orders AS O
+     WHERE O.EmployeeID = E.EmployeeID) AS TotalSales,
+    (SELECT MAX(O.OrderDate)
+     FROM Orders AS O
+     WHERE O.EmployeeID = E.EmployeeID) AS LastOrderDate
+FROM Employees AS E
+WHERE E.EmployeeID IN (
+    SELECT DISTINCT A.EmployeeID
+    FROM Employees AS A
+    INNER JOIN Employees AS B ON A.EmployeeID = B.ReportsTo
+);
+
+-- b)
+SELECT 
+    E.FirstName + ' ' + E.LastName AS 'name', 
+    (SELECT SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount))
+     FROM Orders AS O
+     INNER JOIN [Order Details] AS OD ON O.OrderID = OD.OrderID
+     WHERE E.EmployeeID = O.EmployeeID) +
+    (SELECT SUM(O.Freight)
+     FROM Orders AS O
+     WHERE O.EmployeeID = E.EmployeeID) AS TotalSales,
+    (SELECT MAX(O.OrderDate)
+     FROM Orders AS O
+     WHERE O.EmployeeID = E.EmployeeID) AS LastOrderDate
+FROM Employees AS E
+WHERE E.EmployeeID IN (
+    SELECT DISTINCT A.EmployeeID
+    FROM Employees AS A
+    LEFT JOIN Employees AS B ON A.EmployeeID = B.ReportsTo
+    WHERE B.EmployeeID IS NULL
+);
